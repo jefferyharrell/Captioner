45a0bc9567eaf9bf466e52a710b8b838656bbba5
Jeffery Harrell <jefferyharrell@gmail.com>
2025-04-17 13:58:28 -0700

    Implement dark mode with next-themes and shadcn/ui; fix component import paths; add ModeToggle to main view

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,12 @@
 import Image from "next/image";
+import { ModeToggle } from "@/components/mode-toggle";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[40px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <div className="row-start-1 self-start w-full flex justify-end">
+        <ModeToggle />
+      </div>
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
